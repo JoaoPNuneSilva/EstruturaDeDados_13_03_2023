82f118d8ccbc6ae455bbdd1b50a3803735ab8dc9
JoaoPNuneSilva <joaopedronunesdasilva2@gmail.com>
2023-03-13 20:17:21 -0300

    aula_13_03_2023

---
+++ while2.py
@@ -0,0 +1,9 @@
+contador = 0
+
+while contador <= 100: #enquanto for menor ou igual a 100 o contador roda
+    print(contador)
+    contador = contador +1
+
+    if contador == 24: #se o contador receber o 24, exiba a mensagem abaixo
+        print ("Não vou exibir o número 24")
+        continue
