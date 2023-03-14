2c9a0411eec05411022b5409bf16ec23669572e9
JoaoPNuneSilva <joaopedronunesdasilva2@gmail.com>
2023-03-13 22:21:34 -0300

    aula_13_03_2023

--- calculadora.py
+++ calculadora.py
@@ -1,9 +1,41 @@
-sistema = True #cria o dado boleano
+sair = "Sair"
+while True:
+    # numero1 = (float(input("Escreva o primeiro número: ")))
+    # numero1 = (float(input("Escreva o segundo número: ")))
+    # operador = (float(input("Digite o operador (+-*/): ")))
+    numero1 = input("Escreva o primeiro número: ")
+    numero2 = input("Escreva o segundo número: ")
+    operador = input("Digite o operador (+-*/): ")
 
-while sistema:
-    nome = input ("Escreva o seu nome: ") #cria o dado que recebe um input que vai receber um valor em string
-    print (f"O nome recebido foi: {nome}") #Exibe o nome
+    
+    try: #tentar executar o código
+        numero1_float = float(numero1)
+        numero2_float = float(numero2)
+        nmero_valido= True
 
-    if nome == "Sair":
-        print("Finalizou o sistema")
+    except: #caso tenha e
+        numero_valido= False
+    
+    if numero_valido == False:
+        print("Número inválido. ")
+        continue
+    
+    operador_aceito= ("+-*/")
+    
+    if operador not in operador_aceito:
+        print("Operador não aceito")
+
+    sair = input ("Deseja sair? ").lower().startswith("s")
+    if sair is True:
         break
+    
+    if operador == "+":
+        print(f"{numero1_float} + {numero2} = ", numero1_float + numero2_float)
+    elif operador == "-":
+        print(f"{numero1_float} - {numero2} = ", numero1_float - numero2_float)
+    elif operador == "*":
+        print(f"{numero1_float} * {numero2} = ", numero1_float * numero2_float)
+    elif operador == "/":
+        print(f"{numero1_float} / {numero2} = ", numero1_float / numero2_float)
+    else:
+        print("Error")
